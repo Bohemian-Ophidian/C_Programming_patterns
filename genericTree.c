1f1ac47dda0b7fb39b8f0aff214c3f77ba4856eb
Bohemian-Ophidian <Karan_whistleblower@protonmail.com>
2026-04-08 11:56:58 +0530

    basic functionality added

--- genericTree.c
+++ genericTree.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct NTreeNode {
         int data;
@@ -29,14 +30,71 @@ void addChild(NTreeNode* parent, int data){
         return;
 }
 
+void Traverse(NTreeNode* root){
+        if(root == NULL)
+                return;
+        printf("%d \t", root->data);
+        Traverse(root->firstChild);
+        Traverse(root->nextSibling);
+        return;
+}
+
+void levelOrderTraversal(NTreeNode* root){
+        //pass for now
+}
+
+int getDegree(NTreeNode* parent){
+        int deg = 0;
+        NTreeNode* child = parent->firstChild;
+        while(child != NULL){
+                deg++;
+                child = child->nextSibling;
+        }
+        return deg;
+}
+
+bool isLeaf(NTreeNode* node){
+        if(node->firstChild== NULL)
+                return true;
+        else
+                return false;
+}
+
+NTreeNode* findNode(NTreeNode* parent, int data){
+        if(parent == NULL)
+                return NULL;
+        else if(parent->data == data)
+                return parent;
+        else{
+                NTreeNode* resDepth = findNode(parent->firstChild, data);
+                if(resDepth !=NULL)
+                        return resDepth;
+                else
+                        return findNode(parent->nextSibling, data);
+        }
+        return NULL;
+}
+
+
 int main(void){
         NTreeNode* newnode = NULL;
         newnode = createNode(15);
         addChild(newnode, 17);
         addChild(newnode, 18);
-        printf("%d \n", newnode->data);
-        printf("%d \n", newnode->firstChild->data);
-        printf("%d \n", newnode->firstChild->nextSibling->data);
+        addChild(newnode, 26);
+        addChild(newnode->firstChild, 21);
+        addChild(newnode->firstChild->nextSibling, 22);
+        Traverse(newnode);
+        printf("\n");
+        printf("The degree of the parent is %d", getDegree(newnode));
+        printf("\n");
+        printf("The check for being the leaf node : %d", isLeaf(newnode));
+        printf("\n");
+        printf("The Check for being the leaf node : %d", isLeaf(newnode->firstChild));
+        printf("\n");
+        NTreeNode* check = findNode(newnode, 21);
+        printf("%d", check->data);
+
         return 0;
 }
 
